0a6e19514b1a93a029d47e744684a524e300e44e
VanLance <dylan23smith1990@gmail.com>
2025-04-13 08:42:53 -0500

    first commit

---
+++ CatanGame.Engine/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CatanGame.Engine;
+
+public class Class1
+{
+
+}
---
+++ CatanGame.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CatanGame.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
